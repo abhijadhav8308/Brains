79541962a402674533fc621a2db5b1422f2ee045
Abhishek Jadhav <abhijadhav8308@gmail.com>
2025-03-02 14:28:47 +0530

    Added minor changes in landing page hero component

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -9,20 +9,15 @@ export const LandingHero = () => {
     const { isSignedIn } = useAuth();
 
     return (
-        <div className="text-slate-800 font-bold max-w-screen-xl m-auto py-32 text-center space-y-5">
+        <div className="text-slate-800 font-bold max-w-screen-xl m-auto py-16 md:py-32 px-6 md:px-3 lg:px-3 xl:px-0 text-center space-y-5">
             <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl space-y-5 font-extrabold">
                 <h1>Meet Brains, Your AI Co-Pilot for</h1>
                 <div className="text-transparent bg-clip-text bg-gradient-to-r 
                 from-purple-400 to-pink-600">
                     <TypewriterComponent 
                     options={{
-                        strings: [
-                            "Chatbot.",
-                            "Code Generation.",
-                            "Photo Generation.",
-                            "Music Generation.",
-                            "Video Generation.",
-                            "and everything else...",
+                        strings: ["Chatbot.", "Code Generation.", "Photo Generation.", "Music Generation.", "Video Generation.",
+                            "and Everything Else...",
                         ], autoStart: true, loop: true,
                     }}/>
                 </div>
